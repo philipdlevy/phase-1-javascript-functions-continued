93b8566ea172dc0974048bedc130bfed3ef67d82
Philip Levy <philipdlevy@gmail.com>
2022-02-06 13:33:50 -0700

    commit tree ee086d22843076eb343c889feaf658649d582cfd

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // code your solution here
+function saturdayFun(param1 = "roller-skate") {
+    return `This Saturday, I want to ${param1}!`
+}
+saturdayFun();
+
+function mondayWork(param = "go to the office") {
+    return `This Monday, I will ${param}.`
+}
+mondayWork();
+
+
+function wrapAdjective(flair = "*") {
+    const innerFunction = function (parameter = "special") {
+        return `You are ${flair}${parameter}${flair}!`
+    }
+    return innerFunction;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function wrapAdjective() {
+    const innerFunction = function (parameter = "special");
+      return `You are ${parameter}`
+}
+*/
